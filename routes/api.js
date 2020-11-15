c178f6af2e76916a85d85fba5a7e9662f60e079f
Carlo C <pbjunkiep3@yahoo.com>
2020-11-15 20:19:06 +1100

    did api routes

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+const db = require("../models");
+const router = require("express").Router();
+
+router.get("/api/workouts",(req,res) => {
+    db.Workout.find({})
+        .then((workouts) => {
+            res.json(workouts);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+router.post("/api/workouts", (req,res) => {
+    db.Workout.create({})
+        .then((workout) => {
+            res.json(workout);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+router.put("/api/workouts/:id", (req,res) => {
+    db.Workout.findByIdAndUpdate(
+        req.params.id, { $push: {exercise: req.body }}, {new:true}
+    )
+    .then((workout) => {
+        res.json(workout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req,res) => {
+    db.Workout.find()
+        .then(workout => {
+            res.json(workout)
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+module.exports = router;
